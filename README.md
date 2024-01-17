549c68aa7ec5eb673dce5e82e88813a7dc4bcfa2
Chet Corcos <ccorcos@gmail.com>
2024-01-16 23:14:55 -0800

    Rename to man in the middle

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-# Open Sprinkler with Davis Weather Station
+# Man in the Middle (MITM) Proxy
+
+A simple tool for debugging and reverse engineering by proxying a server and logging all requests and responses.
+
+```sh
+git clone git@github.com:ccorcos/man-in-the-middle.git
+cd man-in-the-middle
+npm i
+chmod +x midm
 
 
-tsx watch --clear-screen=false src/server/server.ts
+# ./midm <domain> <port>
+./midm http://weather.opensprinkler.com 3344
+```
--- server.ts
+++ server.ts
@@ -1,8 +1,20 @@
 import express from "express";
 import helmet from "helmet";
-import morgan from "morgan";
+import { createProxyMiddleware } from "http-proxy-middleware";
 
-const PORT = 3344;
+const args = process.argv.slice(2);
+
+if (args.length !== 2) {
+	console.error(
+		[
+			`Usage: mitm <url> <port>`,
+			`Example: mitm http://weather.opensprinkler.com 3344`,
+		].join("\n")
+	);
+}
+
+const PROXY_URL = args[0];
+const PORT = parseInt(args[1]);
 
 const app = express();
 
@@ -10,20 +22,42 @@ const app = express();
 app.use(helmet());
 
 // Request logging.
-app.use(morgan("dev"));
+// app.use(morgan("dev"));
 
-app.use(express.json({ limit: "4mb" }));
+app.use(express.raw());
 
-// Test endpoint
-app.all("*", (req, res) => {
-  let body = "";
-  if (req.method !== "GET" && req.body) {
-    body = "\n" + JSON.stringify(req.body);
-  }
-  console.log(
-    `${req.method} ${req.path}:\n${JSON.stringify(req.query)}${body}`
-  );
-  res.status(200).send("Good");
+// Log request.
+app.use((req, res, next) => {
+	const message = [req.method, req.path];
+	if (req.query) {
+		message.push(JSON.stringify(req.query));
+	}
+
+	if (req.method !== "GET" && req.body) {
+		message.push(req.body);
+	}
+	console.log("REQUEST:", message.join(" "));
+
+	next();
 });
+
+// Proxy and log response.
+app.use(
+	createProxyMiddleware({
+		target: PROXY_URL,
+		changeOrigin: true,
+		selfHandleResponse: true,
+		onProxyRes: (proxyRes, req, res) => {
+			let body = "";
+			proxyRes.on("data", (chunk) => {
+				body += chunk;
+			});
+			proxyRes.on("end", () => {
+				console.log(`RESPONSE:`, body);
+				res.send(body);
+			});
+		},
+	})
+);
 
 app.listen(PORT, () => console.log(`Listening: http://localhost:${PORT}`));
